83740f3c2bbf38fd2c9be64f8f0c2249d1bfaea5
joal84 <joalmonog@gmail.com>
2023-09-08 15:32:19 +0200

    another test

--- src/components/second-page.jsx
+++ src/components/second-page.jsx
@@ -1,16 +1,14 @@
 import css from "./second-page.module.css";
 import SoundsDetected from "./sounds-detected";
 import StopButton from "./stop-button.jsx";
-import Title from "./title.jsx";
+//import Title from "./title.jsx";
 import DsSelector from "./ds-selector.jsx";
 
 export default function SecondPage() {
   return (
     <div className={css.bg}>
       <div className={css.container}>
-        <div className={css.title}>
-          <Title size="small" />
-        </div>
+        <div className={css.title}></div>
         <SoundsDetected />
         <div className={css.radar}>
           <StopButton />
